e23041efc08c12917b6a1da4d0df50dd13962392
RaulAstudillo06 <astudillo0606@gmail.com>
2021-03-26 09:43:34 -0400

    Make parent_nodes main input of DAG class.

--- boqn/dag.py
+++ boqn/dag.py
@@ -1,18 +1,21 @@
+from typing import List, Optional
+
+
 class DAG(object):
     
-    def __init__(self, dag_list):
-        self.dag_list = dag_list
-        self.n_nodes = len(dag_list)
+    def __init__(self, parent_nodes:List[List[Optional[int]]]):
+        self.parent_nodes = parent_nodes
+        self.n_nodes = len(parent_nodes)
         self.root_nodes = []
         for k in range(self.n_nodes):
-            if len(dag_list[k]) == 0:
+            if len(parent_nodes[k]) == 0:
                 self.root_nodes.append(k)
     
     def get_n_nodes(self):
         return self.n_nodes
     
     def get_parent_nodes(self, k):
-        return self.dag_list[k]
+        return self.parent_nodes[k]
     
     def get_root_nodes(self):
         return self.root_nodes
