bb0c8aac153e66fd943cde1e831bb68f6c246974
RaulAstudillo06 <astudillo0606@gmail.com>
2021-11-05 15:09:31 -0400

    Increase noise level to avoid numerical issues.

--- boqn/initialize_gp_model.py
+++ boqn/initialize_gp_model.py
@@ -5,11 +5,8 @@ from botorch.models.transforms import Standardize
 
 
 def initialize_gp_model(X, Y, Yvar=None):
-    # define model
-    print(X.shape)
-    print(Y.shape)
     if Y.ndim == 1:
         Y = Y.unsqueeze(dim=-1)
-    model = FixedNoiseGP(X, Y, torch.ones(Y.shape) * 1e-6, outcome_transform=Standardize(m=Y.shape[-1]))
+    model = FixedNoiseGP(X, Y, torch.ones(Y.shape) * 1e-5, outcome_transform=Standardize(m=Y.shape[-1]))
     mll = ExactMarginalLogLikelihood(model.likelihood, model)
     return mll, model
